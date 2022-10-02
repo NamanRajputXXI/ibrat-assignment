8a3332860e2b33aa703d4817d46a91765632e152
Naman Rajput <namanrajput801@gmail.com>
2022-10-02 13:36:31 +0530

    chore:[WIP] complete hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,6 +1,27 @@
 import React from "react";
+import strength from "../assets/images/strength.jpg";
+import mobility from "../assets/images/mobility.jpg";
+import drills from "../assets/images/drills.jpg";
 
 const HeroSection = () => {
-  return <div>HeroSection</div>;
+  return (
+    <div className="main__section">
+      <h1>Runners Workout</h1>
+      <div className="main__container">
+        <div className="main__container--content">
+          <h1>Strength</h1>
+          <img src={strength} height="100%" width="100%" alt="drills" />
+        </div>
+        <div className="main__container--content">
+          <h1>Mobility</h1>
+          <img src={mobility} height="100%" width="100%" alt="drills" />
+        </div>
+        <div className="main__container--content">
+          <h1>Drills</h1>
+          <img src={drills} height="100%" width="100%" alt="drills" />
+        </div>
+      </div>
+    </div>
+  );
 };
 export default HeroSection;
